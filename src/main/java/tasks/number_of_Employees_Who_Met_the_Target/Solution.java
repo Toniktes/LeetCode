14caf83db3e32350a96d86b3b45595d1b3ea1982
Anton Teslia <teslyaanton@yandex.ru>
2023-08-01 22:56:17 +0300

    feat: Number of Employees Who Met the Target

--- src/main/java/tasks/number_of_Employees_Who_Met_the_Target/Solution.java
+++ src/main/java/tasks/number_of_Employees_Who_Met_the_Target/Solution.java
@@ -1,4 +1,12 @@
 package tasks.number_of_Employees_Who_Met_the_Target;
 
-public class Solution {
+import java.util.Arrays;
+
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        long count = Arrays.stream(hours)
+                .filter(x -> x >= target)
+                .count();
+        return (int) count;
+    }
 }
